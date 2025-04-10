5c5dfffa931119ce1919161354148ad08639e9ac
ProfitMaxTrader <evansnthiwa@gmail.com>
2025-04-11 02:24:48 +0300

    Update appIdResolver.js
    git commit -m "Update AppIdMap with new bot frontend URLs and application IDs"

--- src/common/appIdResolver.js
+++ src/common/appIdResolver.js
@@ -2,5 +2,7 @@ const AppIdMap = Object.freeze({
     'bot.binary.com': '1169',
     'bot.binary.me' : '15438',
     'binary.bot'    : '15481',
+    'bots.profitmaxtrader.com': '70082', // your bot frontend
+    'localhost': '70082', // optional for local dev
 });
 export default AppIdMap;
